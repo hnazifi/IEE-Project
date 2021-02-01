eec0ef0a65f8f94f4dcff8213e1fea07cd8629e3
kimia <kimiadorrani@gmail.com>
2021-02-01 23:15:01 +0330

    specialities slider (emtiazi)

--- src/app/doctor-single/doctor-single.component.ts
+++ src/app/doctor-single/doctor-single.component.ts
@@ -16,12 +16,12 @@ export class DoctorSingleComponent implements OnInit {
   data;
   stars;
 
-  constructor(private doctorsService: DoctorsService, private router: ActivatedRoute) {
-    this.id = router.snapshot.paramMap.get('id');
+  constructor(private doctorsService: DoctorsService, private router: ActivatedRoute, private renderer: Renderer2) {
+    this.id = router.snapshot.paramMap.get('id'); // get doctor id from route
   }
 
   ngOnInit(): void {
-    this.doctorsService.getDoctorById(this.id).pipe(
+    this.doctorsService.getDoctorById(this.id).pipe( //get doctor detail by id
       catchError((err) => {
         console.log(err);
         window.alert(err.message);
@@ -33,6 +33,29 @@ export class DoctorSingleComponent implements OnInit {
       this.data = res;
       this.stars = Array(this.data.stars).fill(1).map((i) => i);
     });
+  }
+
+  // tab config
+  isDetail: boolean = true;
+
+  isWeekDays: boolean = false;
+
+  showTab(num) {
+    if (num == 0) {
+      this.isDetail = true;
+      this.isWeekDays = false;
+      this.renderer.addClass(document.getElementById('info_tab'), 'actv');
+      this.renderer.removeClass(document.getElementById('info_tab'), 'non_actv');
+      this.renderer.removeClass(document.getElementById('week_tab'), 'actv');
+    }
+    if (num == 1) {
+      this.isDetail = false;
+      this.isWeekDays = true;
+      this.renderer.addClass(document.getElementById('week_tab'), 'actv');
+      this.renderer.removeClass(document.getElementById('week_tab'), 'non_actv');
+      this.renderer.removeClass(document.getElementById('info_tab'), 'actv');
+    }
+
   }
 
 }
